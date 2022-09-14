8463ffb11421208df37d471480110884867d4508
imranahmmed <imranahamed733@gmail.com>
2022-09-15 03:06:08 +0600

    initial commit

---
+++ js/tailwind.config.js
@@ -0,0 +1,27 @@
+tailwind.config = {
+    theme: {
+        extend: {
+            colors: {
+                'clifford': '#da373d',
+                'primary': '#6A4DF4',
+                'tomato': '#FA6444',
+                'green': '#13B237',
+                'yellow': '#F9BD2C',
+                'black': '#151515',
+                'ash': '#737373',
+                'softWhite': '#F8F6FE',
+                'aqua': '#44D4D7'
+            },
+            maxWidth: {
+                'container': '1320px',
+            },
+            fontFamily: {
+                inter: ['Inter', 'sans-serif'],
+            },
+            zIndex: {
+                'zIndex1': '1',
+                'zIndex-1': '-1',
+            }
+        }
+    }
+}
